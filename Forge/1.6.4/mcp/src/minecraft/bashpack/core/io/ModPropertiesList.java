96ab54165431fae2564631f8d0d5d85b07e55623
sinkillerj <techmage1990@yahoo.com>
2013-12-13 03:49:52 -0500

    Update to 1.6.4. Change bacon biome ID to avoid ExtraBiomesXL conflict.

---
+++ Forge/1.6.4/mcp/src/minecraft/bashpack/core/io/ModPropertiesList.java
@@ -0,0 +1,90 @@
+/*******************************************************************************
+ * ModPropertiesList.java
+ * Copyright (c) 2013 WildBamaBoy.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl.html
+ ******************************************************************************/
+
+package bashpack.core.io;
+
+import java.io.Serializable;
+
+/**
+ * Contains the fields used by the mod properties manager.
+ */
+public class ModPropertiesList implements Serializable
+{	
+	public int itemID_RawBacon= 13660;
+	public int itemID_SoggyBacon= 13661;
+	public int itemID_LimpBacon= 13662;
+	public int itemID_CrispyBacon= 13663;
+	public int itemID_BurntBacon= 13664;
+	
+	public int itemID_RawBaconSword= 13668;
+	public int itemID_RawBaconPickaxe= 13669;
+	public int itemID_RawBaconAxe= 13670;
+	public int itemID_RawBaconShovel= 13671;
+	public int itemID_RawBaconHoe= 13672;
+	
+	public int itemID_SoggyBaconSword= 13674;
+	public int itemID_SoggyBaconPickaxe= 13675;
+	public int itemID_SoggyBaconAxe= 13676;
+	public int itemID_SoggyBaconShovel= 13677;
+	public int itemID_SoggyBaconHoe= 13678;
+	
+	public int itemID_LimpBaconSword= 13680;
+	public int itemID_LimpBaconPickaxe= 13681;
+	public int itemID_LimpBaconAxe= 13682;
+	public int itemID_LimpBaconShovel= 13683;
+	public int itemID_LimpBaconHoe= 13684;
+
+	public int itemID_CrispyBaconSword= 13686;
+	public int itemID_CrispyBaconPickaxe= 13687;
+	public int itemID_CrispyBaconAxe= 13688;
+	public int itemID_CrispyBaconShovel= 13689;
+	public int itemID_CrispyBaconHoe= 13690;
+	
+	public int itemID_BurntBaconSword= 13692;
+	public int itemID_BurntBaconPickaxe= 13693;
+	public int itemID_BurntBaconAxe= 13694;
+	public int itemID_BurntBaconShovel= 13695;
+	public int itemID_BurntBaconHoe= 13696;
+	
+	public int itemID_RawBaconHelmet= 13697;
+	public int itemID_RawBaconChestplate= 13698;
+	public int itemID_RawBaconLeggings= 13699;
+	public int itemID_RawBaconBoots= 13700;
+	
+	public int itemID_SoggyBaconHelmet= 13701;
+	public int itemID_SoggyBaconChestplate= 13702;
+	public int itemID_SoggyBaconLeggings= 13703;
+	public int itemID_SoggyBaconBoots= 13704;
+	
+	public int itemID_LimpBaconHelmet= 13705;
+	public int itemID_LimpBaconChestplate= 13706;
+	public int itemID_LimpBaconLeggings= 13707;
+	public int itemID_LimpBaconBoots= 13708;
+	
+	public int itemID_CrispyBaconHelmet= 13709;
+	public int itemID_CrispyBaconChestplate= 13710;
+	public int itemID_CrispyBaconLeggings= 13711;
+	public int itemID_CrispyBaconBoots= 13712;
+	
+	public int itemID_BurntBaconHelmet= 13713;
+	public int itemID_BurntBaconChestplate= 13714;
+	public int itemID_BurntBaconLeggings= 13715;
+	public int itemID_BurntBaconBoots= 13716;
+	
+	public int itemID_BarbecueLighter = 13717;
+	
+	public int blockID_Bone = 232;
+	public int blockID_RawBacon = 233;
+	public int blockID_SoggyBacon = 1443;
+	public int blockID_LimpBacon= 1444;
+	public int blockID_CrispyBacon= 1445;
+	public int blockID_BurntBacon= 1446;
+	public int blockID_BarbecueFire = 1447;
+	public int blockID_Portal = 1448;
+}
